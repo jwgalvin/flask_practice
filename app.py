e478e10858bc7b2c3b0a78eb743cb6197dd753ea
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-20 10:38:22 -0600

    Server responding to postman

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 api = Api(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emp.db'
-app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
 db = SQLAlchemy(app)   
 
